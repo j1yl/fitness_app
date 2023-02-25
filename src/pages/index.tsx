0a8e0cdf6c4ec1c001f38b1141a1fe3634859eb3
Joe Lee <111937630+j1yl@users.noreply.github.com>
2023-02-25 00:35:47 -0800

    Add components and module (#4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import styles from "./index.module.css";
 import { type NextPage } from "next";
 import Head from "next/head";
+import Example from "~/components/Example";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>Fitness App</title>
-        <meta name="description" content="A collection of visually represented fitness exercises." />
+        <meta
+          name="description"
+          content="A collection of visually represented fitness exercises."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Example props={"test"} />
       </main>
     </>
   );
